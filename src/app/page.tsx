88c4b9642524bb743f6e56d61d329940d9d1a380
Éric  António <ericantoniodev@gmail.com>
2026-03-08 13:27:52 +0200

    Depoimentos
    Depimentos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import Depoimentos from "@/components/home/Depoimentos";
 import HeroSection from "@/components/home/HeroSection";
 import Navbar from "@/components/layout/Navbar/Navbar";
 
 export default function Home() {
   return (
     <div className="flex flex-col px-8 bg-background">
-      <Navbar />
-      <HeroSection />
+      <Depoimentos />
     </div>
   );
 }
